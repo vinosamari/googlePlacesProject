b96eb8467a27a8b8492a8d58e3d308c2942e06cf
sapwavino <sapwavinosamari@gmail.com>
2021-06-09 16:52:59 +0100

    Added python-google-places library

--- main.py
+++ main.py
@@ -1,63 +1,26 @@
 #  IMPORTS
-import requests
-import os
-from dotenv import load_dotenv
-from pprint import pprint
 import json
+import os
+from pprint import pprint
+from googleplaces import GooglePlaces
+import requests
+from dotenv import load_dotenv
 
 # LOAD ENVIRONMENT VARIABLES
 load_dotenv()
 
 # VARIABLES
-baseURL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 API_KEY = os.getenv("API_KEY")
-city = "Lagos"
-business = "schools"
-
-# GET THE FIRST PAGE (20 RESULTS)
-pageOne = requests.get(
-    f"{baseURL}?query={business}+in+{city}&key={API_KEY}")
-pageOneResults = pageOne.json()["results"]
-
-# GET THE NEXT PAGE'S TOKEN
-nextPageToken = pageOne.json()["next_page_token"]
-
+google_places = GooglePlaces(API_KEY)
+hasNextPageToken = False
+textQuery = 'studio in Abuja'
 resultsArray = []
-counter = 0
+currentPageResults = google_places.text_search(query=textQuery)
 
-# ADD RESULTS TO ARRAY
-for result in pageOneResults:
-    resultsArray.append(result)
-    counter += 1
-
-# RECURSIVELY CHECK FOR THE NEXT PAGE
-
-
-def getNextPage(pageToken):
-    # check if the page has a next_page_token
-    # fetch the next page results with the token
-    # add it to the json file
-    # increment the counter
-    # else do nothing. i guess.
-    pass
-
-
-if nextPageToken:
-    print()
-    print("Next Page")
-    print()
-    nextPage = requests.get(
-        f"{baseURL}?query={business}+in+{city}&pagetoken={nextPageToken}&key={API_KEY}")
-    nextPageResults = nextPage.json()["results"]
-    # pprint(nextPage.json())
-    for result in pageOneResults:
-        resultsArray.append(result)
-        counter += 1
-
-
-# WRITE TO JSON FILE
-with open('results.json', 'a+') as res:
-    json.dump(resultsArray, res)
-
-print(f'{counter} items in the results')
-print("DONE! ✅")
+#  CHECK FOR NEXT PAGE
+if currentPageResults.has_next_page_token:
+    nextPageResults = google_places.text_search(
+        query=textQuery, pagetoken=currentPageResults.next_page_token)
+# DISPLAY NEXT PAGE
+pprint(currentPageResults.places)
+pprint(nextPageResults.places)
