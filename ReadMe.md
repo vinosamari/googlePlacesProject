31ab9870b6ec0cd38dbc71b80eed1be44d4c2928
sapwavino <sapwavinosamari@gmail.com>
2021-06-09 21:51:58 +0100

    Fetches data w search params + dumps to .json file

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+#  Quickstart
+<hr>
+
+1. Start up your environment and pip install the requirements
+2. Edit the query params in main.py
+3. run the main.py file
+4. open Search.json for your results
--- main.py
+++ main.py
@@ -1,26 +1,85 @@
 #  IMPORTS
 import json
 import os
+import time
 from pprint import pprint
-from googleplaces import GooglePlaces
+
 import requests
 from dotenv import load_dotenv
 
-# LOAD ENVIRONMENT VARIABLES
+# LOAD ENVIRONMENT VARIABLES FROM .env
 load_dotenv()
 
 # VARIABLES
+baseURL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 API_KEY = os.getenv("API_KEY")
-google_places = GooglePlaces(API_KEY)
-hasNextPageToken = False
-textQuery = 'studio in Abuja'
 resultsArray = []
-currentPageResults = google_places.text_search(query=textQuery)
+numberOfQueries = 6
+counter = 0
+page = 0
 
-#  CHECK FOR NEXT PAGE
-if currentPageResults.has_next_page_token:
-    nextPageResults = google_places.text_search(
-        query=textQuery, pagetoken=currentPageResults.next_page_token)
-# DISPLAY NEXT PAGE
-pprint(currentPageResults.places)
-pprint(nextPageResults.places)
+
+# QUERY PARAMS
+category = "a domicile"
+location = "Prigonrieux"
+
+
+# REQUEST TIME
+requestTime = time.perf_counter()
+
+
+# GET THE FIRST PAGE (20 RESULTS)
+currentPage = requests.get(
+    f"{baseURL}?query={category}+in+{location}&key={API_KEY}")
+currentPageResults = currentPage.json()["results"]
+
+print("✈️ Fetching First Page")
+page += 1
+
+for result in currentPageResults:
+    resultsArray.append(result)
+    counter += 1
+print("✅ Done Fetching First Page")
+
+#  GET SUBSEQUENT PAGES BASED ON LIST OF QUERIES
+
+for i in range(numberOfQueries):
+    page += 1
+    print(f"✈️ Fetching Page: {page}")
+
+    if currentPage.json()["next_page_token"]:
+        nextPageToken = currentPage.json()["next_page_token"]
+        nextPage = requests.get(
+            f"{baseURL}?query={category}+in+{location}&pagetoken={nextPageToken}&key={API_KEY}")
+        nextPageResults = nextPage.json()["results"]
+
+        # ADD RESULTS TO ARRAY
+        for result in nextPageResults:
+            resultsArray.append(result)
+            counter += 1
+
+    else:
+        print("❌ No Other Pages Available")
+
+print(f"✅ Done Fetching All {page} Pages")
+
+#  GET ALL THE PLACES IN THE LOCATION (PLACES FROM ALL PAGES)
+print("✈️ Fetching All Places")
+placeData = {}
+with open('Search.json', 'a') as f:
+    for place in resultsArray:
+        placeData["Name"] = place.get("name")
+        placeData["ID"] = place.get("place_id")
+        placeData["Address"] = place.get("formatted_address")
+        placeData["Rating"] = place.get("rating")
+        json.dump(placeData, f)
+
+
+print(f"✅ Done Stacking All {len(resultsArray)} Places")
+
+
+# RESPONSE TIME
+responseTime = time.perf_counter()
+
+print(f"finished in {responseTime - requestTime:.2f}s")
+print("✅ Done!")
