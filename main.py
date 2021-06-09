e80b941a1db630640ba6f70657951e9947c598b7
sapwavino <sapwavinosamari@gmail.com>
2021-06-09 13:24:58 +0100

    Minor Fixes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#  IMPORTS
 import requests
 import os
 from dotenv import load_dotenv
@@ -10,21 +11,52 @@ load_dotenv()
 # VARIABLES
 baseURL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 API_KEY = os.getenv("API_KEY")
-city = "Périgueux"
-business = "hotels"
-resp = requests.get(f"{baseURL}?query={business}+in+{city}&key={API_KEY}")
-results = resp.json()["results"]
+city = "Lagos"
+business = "schools"
+
+# GET THE FIRST PAGE (20 RESULTS)
+pageOne = requests.get(
+    f"{baseURL}?query={business}+in+{city}&key={API_KEY}")
+pageOneResults = pageOne.json()["results"]
+
+# GET THE NEXT PAGE'S TOKEN
+nextPageToken = pageOne.json()["next_page_token"]
+
 resultsArray = []
 counter = 0
 
-for result in results:
+# ADD RESULTS TO ARRAY
+for result in pageOneResults:
     resultsArray.append(result)
     counter += 1
+
+# RECURSIVELY CHECK FOR THE NEXT PAGE
+
+
+def getNextPage(pageToken):
+    # check if the page has a next_page_token
+    # fetch the next page results with the token
+    # add it to the json file
+    # increment the counter
+    # else do nothing. i guess.
+    pass
+
+
+if nextPageToken:
+    print()
+    print("Next Page")
+    print()
+    nextPage = requests.get(
+        f"{baseURL}?query={business}+in+{city}&pagetoken={nextPageToken}&key={API_KEY}")
+    nextPageResults = nextPage.json()["results"]
+    # pprint(nextPage.json())
+    for result in pageOneResults:
+        resultsArray.append(result)
+        counter += 1
 
 
 # WRITE TO JSON FILE
 with open('results.json', 'a+') as res:
-    # res.write(str(resultsArray))
     json.dump(resultsArray, res)
 
 print(f'{counter} items in the results')
